63e0f467f3a9c68bb58c9b99871a0ff71e4b4f95
R.Ramos <rramosparaiso@gmail.com>
2016-11-28 17:28:22 +0100

    .

--- Duende/routes/market-routes.js
+++ Duende/routes/market-routes.js
@@ -4,25 +4,25 @@ var index_list = [
         name: 'IBEX-35',
         symbol: '^IBEX',
         growth: 5.38,
-        value: 10892,
+        value: 10.892,
         description: 'Mercado Español'
     }, {
         name: 'NASDAQ',
         symbol: '^NASDAQ',
         growth: -1.87,
-        value: 18930,
+        value: 18.931,
         description: 'Mercado amerciano'
     }, {
         name: 'DAX-50',
         symbol: '^DAX',
         growth: -2.41,
-        value: 12374,
+        value: 12.374,
         description: 'Mercado aleman'
     }, {
         name: 'NIKKEI-225',
         symbol: '^NIKKEI',
         growth: 0.79,
-        value: 4859,
+        value: 4.859,
         description: 'Mercado japones'
     }
 ];
